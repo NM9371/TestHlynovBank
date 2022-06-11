9edf8d8c5191d3f010d479d350e6535ff5f21af1
N371 <NM9371@gmail.com>
2022-06-12 01:39:06 +0300

    grid

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,28 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.NameTextBox = new System.Windows.Forms.TextBox();
-            this.SurnameTextBox = new System.Windows.Forms.TextBox();
             this.EnterButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
             this.SuspendLayout();
-            // 
-            // NameTextBox
-            // 
-            this.NameTextBox.Location = new System.Drawing.Point(323, 70);
-            this.NameTextBox.Name = "NameTextBox";
-            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
-            this.NameTextBox.TabIndex = 1;
-            // 
-            // SurnameTextBox
-            // 
-            this.SurnameTextBox.Location = new System.Drawing.Point(323, 110);
-            this.SurnameTextBox.Name = "SurnameTextBox";
-            this.SurnameTextBox.Size = new System.Drawing.Size(100, 20);
-            this.SurnameTextBox.TabIndex = 2;
             // 
             // EnterButton
             // 
-            this.EnterButton.Location = new System.Drawing.Point(335, 156);
+            this.EnterButton.Location = new System.Drawing.Point(84, 342);
             this.EnterButton.Name = "EnterButton";
             this.EnterButton.Size = new System.Drawing.Size(75, 23);
             this.EnterButton.TabIndex = 3;
@@ -57,25 +47,73 @@
             this.EnterButton.UseVisualStyleBackColor = true;
             this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.DisplayMember = "fileEntries";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(131, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.ValueMember = "fileEntries";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // grid
+            // 
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Location = new System.Drawing.Point(12, 118);
+            this.grid.Name = "grid";
+            this.grid.Size = new System.Drawing.Size(240, 186);
+            this.grid.TabIndex = 17;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Данные";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Значение";
+            this.Column2.Name = "Column2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Название шаблона";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(324, 407);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.EnterButton);
-            this.Controls.Add(this.SurnameTextBox);
-            this.Controls.Add(this.NameTextBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Заполнение шаблона";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox NameTextBox;
-        private System.Windows.Forms.TextBox SurnameTextBox;
         private System.Windows.Forms.Button EnterButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,40 +10,78 @@ using System.Windows.Forms;
 using Word = Microsoft.Office.Interop.Word;
 using System.Reflection;
 using System.IO;
+using System.Linq;
 
 namespace TestHlynovBank
 {
     public partial class Form1 : Form
     {
+        string[] samplePaths { get; set; }
+        Word._Application oWord;
+        Word._Document oDoc;
+        BindingSource source = new BindingSource();
+
+        private Dictionary<string, string> FillingData = new Dictionary<string, string>();
         public Form1()
         {
             InitializeComponent();
+            samplePaths = Directory.GetFiles(Directory.GetCurrentDirectory() + @"\samples");
+            string[] sampleNames = samplePaths.Select(path => Path.GetFileNameWithoutExtension(path)).ToArray();
+            comboBox1.DataSource = sampleNames;
+
         }
-        private void FillingSample()
+        private void LoadSample(string i)
         {
-            Word._Application oWord;
-            Word._Document oDoc;
+            //CloseWord();
             Object oMissing = System.Reflection.Missing.Value;
             Object oTrue = true;
             Object oFalse = false;
             //создаем обьект приложения word
             oWord = new Word.Application();
             // создаем путь к файлу
-            Object oTemplate = "D:\\Job\\C#\\TestHlynovBank\\samples\\sample2.dotx";
+            Object oTemplate = samplePaths[0];
             oDoc = oWord.Documents.Add(ref oTemplate, ref oMissing, ref oMissing, ref oMissing);
-
-            oDoc.Bookmarks["DataName"].Range.Text = NameTextBox.Text;
-            oDoc.Bookmarks["DataName"].Delete();
-            oDoc.Bookmarks["DataSurname"].Range.Text = SurnameTextBox.Text;
-            oDoc.Bookmarks["DataSurname"].Delete();
             oWord.Visible = true;
+            FillingData.Clear();
+            foreach (dynamic bm in oDoc.Bookmarks)
+            {
+                dynamic bname = bm.Name;
+                FillingData.Add(bname,"");
+            }
+            source.DataSource = FillingData;
+            grid.DataSource = source;
+            grid.Refresh();
+        }
+        private void FillingSample()
+        {
+            oWord.Visible = false;
+            foreach (var fd in FillingData)
+            {
+                oDoc.Bookmarks[fd].Range.Text = FillingData.Values.ToString();
+                oDoc.Bookmarks[fd].Delete();
+            }
+            oWord.Visible = true;
+        }
 
+        private void CloseWord()
+        {
+            oDoc.Close();
+            oWord.Quit();
         }
 
         private void EnterButton_Click(object sender, EventArgs e)
         {
-            //File.Open("..\\samples\\a.txt", FileMode.Open);
             FillingSample();
+        }
+
+        private void Form1_Closing(object sender, EventArgs e)
+        {
+            CloseWord();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadSample(samplePaths[comboBox1.SelectedIndex]);
         }
     }
 }
